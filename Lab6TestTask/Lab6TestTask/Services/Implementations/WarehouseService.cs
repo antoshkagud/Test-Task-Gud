32e95b0f477f0749fef5f3f414fd24cfa1cb887b
antoshkagud <antoshkagud@gmail.com>
2025-10-15 03:00:06 +0300

    compleated

--- Lab6TestTask/Lab6TestTask/Services/Implementations/WarehouseService.cs
+++ Lab6TestTask/Lab6TestTask/Services/Implementations/WarehouseService.cs
@@ -7,7 +7,34 @@ namespace Lab6TestTask.Services.Implementations;
 /// <summary>
 /// WarehouseService.
 /// Implement methods here.
-/// </summary>
+/// </summary>\
+
+public async Task<Warehouse> GetWarehouseAsync()
+{
+    Warehouse wHouseMaxSum = await (from prod in _dbContext.Products
+                                    where prod.Status == ProductStatus.ReadyForDistribution
+                                    group prod by prod.WarehouseId into gr
+                                    let totalValue = gr.Sum(prod => prod.Price * prod.Quantity)
+                                    orderby totalValue descending
+                                    select gr.Key into warehouseId
+                                    join wHouse in _dbContext.Warehouses on warehouseId equals wHouse.Id
+                                    select wHouse
+                                    ).FirstOrDefaultAsync();
+
+    return wHouseMaxSum;
+}
+
+// TODO:Сделать перегрузку на первый метод с выходом списка
+
+public async Task<IEnumerable<Warehouse>> GetWarehousesAsync()
+{
+    List<Warehouse> wHouseSecQuarter = await (from prod in _dbContext.Products
+                                              where prod.DeliveryDate >= new DateTime(2025, 4, 1) && prod.DeliveryDate <= new DateTime(2025, 6, 30)
+                                              select prod.Warehouse).Distinct().ToListAsync();
+
+    return wHouseSecQuarter;
+}
+
 public class WarehouseService : IWarehouseService
 {
     private readonly ApplicationDbContext _dbContext;
